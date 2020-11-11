7d817bcbb545861bd61c4b57730a749a8e354478
Nina Dissard <dissardnina@gmail.com>
2020-11-11 13:01:28 +0100

    Implement validation methods for restaurant and review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  # class Pet < ApplicationRecord
+  #   validates :name, presence: true
+  #   validates :species, inclusion: { in: %w(dog cat rabbit turtle snake) }
+  #  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  # rating compris entre 0 et 5
+  validates :rating, inclusion: { in: (0..5).to_a }, numericality: { only_integer: true }
 end
