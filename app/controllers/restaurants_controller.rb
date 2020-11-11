098c01e2f05aa6b49c4a5323858ba412f7c759e7
Nina Dissard <dissardnina@gmail.com>
2020-11-11 16:03:26 +0100

    Start the reviews controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,4 @@
 class RestaurantsController < ApplicationController
-  # before_action :set_restaurant, only: [:show]
 
   def index
     @restaurants = Restaurant.all
@@ -20,10 +19,6 @@ class RestaurantsController < ApplicationController
   end
 
   private
-
-  # def set_restaurant
-  #   @restaurant = Restaurant.find(params[:id])
-  # end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :category, :phone_number)
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
 
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.save
+    redirect_to review_path(@review)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 
 end
